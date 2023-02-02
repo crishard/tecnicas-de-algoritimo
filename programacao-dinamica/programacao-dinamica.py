90089e7944868f1615e74b22f8c666870eb10e70
crishard <crislantorres9@gmail.com>
2023-02-02 10:44:48 -0300

    Melhorando a exibição dos resultados no terminal

--- programacao-dinamica/programacao-dinamica.py
+++ programacao-dinamica/programacao-dinamica.py
@@ -1,22 +1,26 @@
 def dpMakeChange(coinValueList,change,minCoins,coinsUsed):
-   for cents in range(change+1):
+   for cents in range(change+1): #n + 1
       coinCount = cents
       newCoin = 1
-      for j in [c for c in coinValueList if c <= cents]:
+      for j in [c for c in coinValueList if c <= cents]: #m
             if minCoins[cents-j] + 1 < coinCount:
                coinCount = minCoins[cents-j]+1
                newCoin = j
       minCoins[cents] = coinCount
       coinsUsed[cents] = newCoin
+
+   print("Número de moedas para troco de cada valor até o valor do troco \n", minCoins, "\n")
    return minCoins[change]
 
 def printCoins(coinsUsed,change):
-   print(coinsUsed)
+   coinsFinally = []
    coin = change
    while coin > 0:
       thisCoin = coinsUsed[coin]
-      print(thisCoin)
+      coinsFinally.append(thisCoin)
       coin = coin - thisCoin
+   print("Moedas utilizadas para o troco: ", coinsFinally, '\n')
+   print("Maior moeda utilizada nos testes para cada valor \n", coinsUsed, '\n')
 
 def main():
 
@@ -25,9 +29,7 @@ def main():
    coinsUsed = [0]*(change+1)
    coinCount = [0]*(change+1)
 
-   print("Fazendo troco para",change,"requer")
-   print(dpMakeChange(coinsList,change,coinCount,coinsUsed),"cédulas")
-   print("Elas são:")
-   printCoins(coinsUsed,change)
+   print("Fazendo troco para",change,"requer", dpMakeChange(coinsList,change,coinCount,coinsUsed),"moedas")
+   printCoins(coinsUsed, change)
 
 main()
